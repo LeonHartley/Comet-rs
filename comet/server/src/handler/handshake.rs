1e3bf9f6a2545780d7816da0311abb54b698cb3f
Leon Hartley <lhartley97@gmail.com>
2018-12-25 11:21:27 +0000

    nicer handshake handler

--- comet/server/src/handler/handshake.rs
+++ comet/server/src/handler/handshake.rs
@@ -13,11 +13,11 @@ pub fn client_version_handler(buf: &mut Buffer, _: Addr<ServerSession>) {
 }
 
 pub fn authentication_handler(buf: &mut Buffer, session: Addr<ServerSession>) {
-    match buf.read_string() {
-        Some(s) => {
-            session.do_send(ComposeMessage(auth_ok_composer()));
-            session.do_send(ComposeMessage(motd_composer(format!("got ticket: {}", s))));
-        }
-        _ => return
+    let ticket = match buf.read_string() {
+        Some(s) => s,
+        None => return
     };
+
+    session.do_send(ComposeMessage(auth_ok_composer()));
+    session.do_send(ComposeMessage(motd_composer(format!("got ticket: {}", ticket))));
 }
