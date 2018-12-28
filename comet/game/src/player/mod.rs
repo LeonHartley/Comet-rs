b8807c7bec7fc333a8668b19d5f4dcf6779a335a
Leon Hartley <lhartley97@gmail.com>
2018-12-28 15:13:10 +0000

    send login messages

--- comet/game/src/player/mod.rs
+++ comet/game/src/player/mod.rs
@@ -1,6 +1,8 @@
 use actix::{Actor, Message, Recipient, Context};
 use model::player;
 use protocol::buffer::StreamMessage;
+use protocol::composer::handshake::auth_ok_composer;
+use protocol::composer::handshake::motd_composer;
 
 pub struct Player {
     stream: Recipient<StreamMessage>,
@@ -25,6 +27,9 @@ impl Actor for Player {
     type Context = Context<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
-        println!("Player {} logged in", self.data().name);
+        self.stream.do_send(StreamMessage::BufferedSend(vec![
+            auth_ok_composer(),
+            motd_composer(String::from("top kek"))
+        ]));
     }
 }
