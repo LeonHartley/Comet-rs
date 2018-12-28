948e15df5dc7ce58e6ff19277195699287b669cf
Leon Hartley <lhartley97@gmail.com>
2018-12-28 19:29:30 +0000

    nicer

--- comet/server/src/handler/player.rs
+++ comet/server/src/handler/player.rs
@@ -3,6 +3,6 @@ use handler::req::player::InfoRetrieve;
 use protocol::buffer::Buffer;
 use session::ServerSession;
 
-pub fn info_retrieve(buffer: &mut Buffer, session: Addr<ServerSession>) {
+pub fn info_retrieve(_: &mut Buffer, session: Addr<ServerSession>) {
     session.do_send(InfoRetrieve);
 }
--- comet/server/src/handler/req/player.rs
+++ comet/server/src/handler/req/player.rs
@@ -1,9 +1,4 @@
 use actix::{Context, Handler, Message};
-use actix::ActorFuture;
-use actix::fut::{ok, WrapFuture};
-use actix::prelude::*;
-use db::query::player::PlayerByLoginTicket;
-use protocol::buffer::StreamMessage;
 use protocol::composer::player::credits_composer;
 use protocol::composer::player::player_info_composer;
 use session::ServerSession;
@@ -14,7 +9,7 @@ pub struct InfoRetrieve;
 impl Handler<InfoRetrieve> for ServerSession {
     type Result = ();
 
-    fn handle(&mut self, msg: InfoRetrieve, ctx: &mut Context<Self>) {
+    fn handle(&mut self, _: InfoRetrieve, _: &mut Context<Self>) {
         let player = match self.player_data() {
             Some(p) => p,
             _ => return
