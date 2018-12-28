69f5aa2107c83ab4b28984e4891542af5be647ec
Leon Hartley <lhartley97@gmail.com>
2018-12-28 12:11:19 +0000

    start player module

--- comet/game/src/lib.rs
+++ comet/game/src/lib.rs
@@ -1,2 +1,5 @@
+extern crate protocol;
+extern crate model;
+extern crate actix;
 
 pub mod player;
--- comet/game/src/player/mod.rs
+++ comet/game/src/player/mod.rs
@@ -1,3 +1,18 @@
+use actix::Recipient;
+use model::player;
+use protocol::buffer::StreamMessage;
+
 pub struct Player {
-    stream: Recipient<StreamMessage>
+    stream: Recipient<StreamMessage>,
+    inner: player::Player,
+}
+
+impl Player {
+    pub fn data_mut(&mut self) -> &mut player::Player {
+        &mut self.inner
+    }
+
+    pub fn data(&self) -> &player::Player {
+        &self.inner
+    }
 }
