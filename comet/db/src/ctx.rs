3c99665fd2c38ea97f77937f7e8aa4d46b295d95
Leon Hartley <lhartley97@gmail.com>
2018-12-28 11:59:29 +0000

    cleanup

--- comet/db/src/ctx.rs
+++ comet/db/src/ctx.rs
@@ -6,3 +6,9 @@ pub struct DbContext(pub mysql::Pool);
 impl Actor for DbContext {
     type Context = SyncContext<Self>;
 }
+
+impl DbContext {
+    pub fn pool(&mut self) -> mysql::Pool {
+        self.0.clone()
+    }
+}
--- comet/db/src/query/player.rs
+++ comet/db/src/query/player.rs
@@ -17,23 +17,26 @@ impl Handler<PlayerByLoginTicket> for DbContext {
     fn handle(&mut self, msg: PlayerByLoginTicket, ctx: &mut SyncContext<Self>) -> Self::Result {
         println!("Getting player data by ticket: {}", msg.0);
 
-        let mut r: Result<Vec<Player>, _> = self.0.prep_exec("SELECT id, username AS name, figure, motto
-        FROM players WHERE auth_ticket = :ticket;", params! {
-            "ticket" => msg.0,
-        }).map(|res| {
-            res.map(|x| x.unwrap()).map(|row| {
-                let (id, name, figure, motto) = mysql::from_row(row);
+        let result: Result<Vec<Player>, _> = self.pool()
+            .prep_exec("SELECT id, username AS name, figure, motto
+                              FROM players WHERE auth_ticket = :ticket;",
+                       params! {
+                                "ticket" => msg.0,
+            })
+            .map(|res| {
+                res.map(|x| x.unwrap()).map(|row| {
+                    let (id, name, figure, motto) = mysql::from_row(row);
 
-                Player {
-                    id,
-                    name,
-                    figure,
-                    motto,
-                }
-            }).collect()
-        });
+                    Player {
+                        id,
+                        name,
+                        figure,
+                        motto,
+                    }
+                }).collect()
+            });
 
-        if let Ok(players) = r {
+        if let Ok(players) = result {
             players
                 .into_iter()
                 .next()
