154aeadace614551601e9ae29ae8aad1195ba515
Leon Hartley <lhartley97@gmail.com>
2019-01-02 20:44:51 +0000

    much nicer component system

--- comet/game/src/container/mod.rs
+++ comet/game/src/container/mod.rs
@@ -1,28 +1,41 @@
 use std::any::{Any, TypeId};
+use std::boxed::*;
 use std::collections::HashMap;
 use std::mem::transmute;
+use std::ops::Deref;
 use std::sync::Arc;
 
-pub type ComponentSet = HashMap<TypeId, Arc<Component>>;
+pub type ComponentSet = HashMap<TypeId, Box<Any>>;
 
 pub trait Container {
     fn components(&self) -> &ComponentSet;
     fn components_mut(&mut self) -> &mut ComponentSet;
 
-    fn add_component(&mut self, component: Arc<Component>) {
-        self.components_mut()
-            .insert(component.typeid(), component);
+    fn add_component(&mut self, component: Box<Component>) {
+        self.components_mut().insert(component.typeid(), unsafe {
+            transmute::<Box<Component>, Box<Any>>(component)
+        });
     }
 
-    fn component<T: 'static>(&self) -> Arc<T> where T: Component {
-        let component = Box::new(self
-            .components()
-            .get(&TypeId::of::<T>())
-            .expect("Components are not configured!")
-            .clone());
-
+    fn component<T: 'static>(&self) -> &T where T: Component {
         unsafe {
-            *transmute::<Box<Arc<Component>>, Box<Arc<T>>>(component)
+            &**self
+                .components()
+                .get(&TypeId::of::<T>())
+                .expect("Components are not configured!")
+                .downcast_ref::<*mut T>()
+                .expect("Invalid component type")
+        }
+    }
+
+    fn component_mut<T: 'static>(&mut self) -> &mut T where T: Component {
+        unsafe {
+            &mut **self
+                .components_mut()
+                .get_mut(&TypeId::of::<T>())
+                .expect("Components are not configured!")
+                .downcast_mut::<*mut T>()
+                .expect("Invalid component type")
         }
     }
 }
--- comet/game/src/player/service.rs
+++ comet/game/src/player/service.rs
@@ -1,11 +1,10 @@
-use std::collections::HashMap;
-use std::sync::Mutex;
-
 use container::{Component, Container};
+use core::borrow::BorrowMut;
 use ctx::GameContext;
 use player::Player;
-use core::borrow::BorrowMut;
+use std::collections::HashMap;
 use std::sync::Arc;
+use std::sync::Mutex;
 
 pub trait PlayerService {
     fn is_online(&self, player_id: i64) -> bool;
@@ -33,11 +32,14 @@ impl PlayerService for PlayerServiceContext {
 }
 
 impl PlayerService for GameContext {
-    fn is_online(&self, player_id: i64) -> bool {
-        unimplemented!()
+    fn is_player_online(&self, player_id: i64) -> bool {
+        self.component::<PlayerServiceContext>()
+            .is_online(player_id)
     }
 
+
     fn on_player_online(&mut self, player: &Player) {
-        unimplemented!()
+        self.component_mut::<PlayerServiceContext>()
+            .on_player_online(&player)
     }
 }
