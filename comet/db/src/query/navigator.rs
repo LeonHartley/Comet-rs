b696a0578b91102ff997c01e61a0c607f4488fa7
Leon Hartley <leonhartley@ekmsystems.co.uk>
2019-01-03 22:29:17 +0000

    reorder

--- comet/db/src/query/navigator.rs
+++ comet/db/src/query/navigator.rs
@@ -5,26 +5,6 @@ pub trait NavigatorRepository {
     fn get_navigator_categories(&mut self) -> Option<Vec<Category>>;
 }
 
-impl NavigatorRepository for DbContext {
-    fn get_navigator_categories(&mut self) -> Option<Vec<Category>> {
-        match self
-            .pool()
-            .prep_exec("SELECT id, category, category_identifier AS category_id, public_name AS name, required_rank AS player_rank, view_mode, category_type, search_allowance AS search_option, room_count, room_count_expanded, visible FROM navigator_categories;", ())
-            .map(|res| {
-                res.map(|x| x.unwrap()).map(|row| {
-                    let (id, category, category_id, name, player_rank, view_mode, category_type, search_option, room_count, room_count_expanded, visible) = mysql::from_row(row);
-                    CategoryQueryResult { id, category, category_id, name, player_rank, view_mode, category_type, search_option, room_count, room_count_expanded, visible }.into()
-                }).collect()
-            }) {
-            Ok(categories) => Some(categories),
-            Err(e) => {
-                error!("MySQL Error {:?}", e);
-                None
-            }
-        }
-    }
-}
-
 struct CategoryQueryResult {
     id: i32,
     category: String,
@@ -53,6 +33,26 @@ impl Into<Category> for CategoryQueryResult {
             room_count: self.room_count,
             room_count_expanded: self.room_count_expanded,
             visible: self.visible,
+        }
+    }
+}
+
+impl NavigatorRepository for DbContext {
+    fn get_navigator_categories(&mut self) -> Option<Vec<Category>> {
+        match self
+            .pool()
+            .prep_exec("SELECT id, category, category_identifier AS category_id, public_name AS name, required_rank AS player_rank, view_mode, category_type, search_allowance AS search_option, room_count, room_count_expanded, visible FROM navigator_categories;", ())
+            .map(|res| {
+                res.map(|x| x.unwrap()).map(|row| {
+                    let (id, category, category_id, name, player_rank, view_mode, category_type, search_option, room_count, room_count_expanded, visible) = mysql::from_row(row);
+                    CategoryQueryResult { id, category, category_id, name, player_rank, view_mode, category_type, search_option, room_count, room_count_expanded, visible }.into()
+                }).collect()
+            }) {
+            Ok(categories) => Some(categories),
+            Err(e) => {
+                error!("MySQL Error {:?}", e);
+                None
+            }
         }
     }
 }
