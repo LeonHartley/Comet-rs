5f644e8c2170a31b01ea49d75a3db71272f296c3
Leon Hartley <lhartley97@gmail.com>
2019-04-23 20:59:25 +0100

    that's much better

--- comet/server/src/handler/navigator.rs
+++ comet/server/src/handler/navigator.rs
@@ -1,18 +1,27 @@
-use protocol::buffer::Buffer;
+use protocol::buffer::{Buffer, StreamMessage};
 use session::ServerSession;
 use handler::req::navigator::RoomCategories;
-use actix::{Context, Addr};
+use actix::{Context, Addr, AsyncContext};
 use handler::req::navigator::InitialiseNavigator;
 use futures::sink::Sink;
+use game::navigator::service::NavigatorService;
+use protocol::composer::navigator::{room_categories_composer, navigator_metadata_composer, navigator_settings_composer};
 
-pub fn room_categories_handler(buf: &mut Buffer, session: &mut ServerSession, _: &mut Context<ServerSession>) {
-    if let Some(ref ctx) = session.player {
-        ctx.addr.do_send(RoomCategories);
+pub fn room_categories_handler(buf: &mut Buffer, session: &mut ServerSession, context: &mut Context<ServerSession>) {
+    if let Some(ctx) = &session.player {
+        if let Ok(player) = ctx.data.read() {
+            let _ = context.address().do_send(StreamMessage::Send(
+                room_categories_composer(session.game.get_room_categories(), player.rank)));
+        }
     }
 }
 
-pub fn initialise_handler(buf: &mut Buffer, session: &mut ServerSession, _: &mut Context<ServerSession>) {
-    if let Some(ref ctx) = session.player {
-        ctx.addr.do_send(InitialiseNavigator);
+pub fn initialise_handler(buf: &mut Buffer, session: &mut ServerSession, context: &mut Context<ServerSession>) {
+    if let Some(ctx) = &session.player {
+        if let Ok(player) = ctx.data.read() {
+            let _ = context.address().do_send(StreamMessage::BufferedSend(vec![
+                navigator_settings_composer(&player.settings.navigator),
+                navigator_metadata_composer()]));
+        }
     }
 }
